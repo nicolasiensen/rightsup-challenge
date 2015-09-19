a0f115e9db0a9d1e0544f7ba596481fded73f4c2
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-19 08:31:26 -0300

    Change test description in RightHolder spec

--- spec/right_holder_spec.rb
+++ spec/right_holder_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require "#{File.dirname(__FILE__)}/../src/right_holder"
 
 RSpec.describe RightHolder do
-  it 'should have a name' do
+  it 'has a name' do
     rightsup = RightHolder.new('rightsup')
     expect(rightsup.name).to be_eql('rightsup')
   end
