39ca72d51cfb8a5f6e063972da9dc7d535720cca
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-20 08:16:39 -0300

    Add support to business introducer to the LicenseAgreement class

--- spec/entities/license_agreement_spec.rb
+++ spec/entities/license_agreement_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe LicenseAgreement do
     expect(license_agreement.rh2).to be_eql(rightsup)
     expect(license_agreement.royalty_percentage).to be_eql(royalty_percentage)
   end
+
+  it 'could have a business introducer' do
+    rightsup = RightHolder.new('rightsup')
+    rh2 = RightHolder.new('rh2')
+    royalty_percentage = RoyaltyPercentage.new(20.0)
+    bi_percentage = RoyaltyPercentage.new(50.0)
+
+    license_agreement = LicenseAgreement.new(
+      rh2,
+      rightsup,
+      royalty_percentage,
+      bi_percentage
+    )
+
+    expect(license_agreement.bi_percentage).to be_eql(bi_percentage)
+  end
 end
--- src/entities/license_agreement.rb
+++ src/entities/license_agreement.rb
@@ -1,7 +1,7 @@
 class LicenseAgreement
-  attr_accessor :rh1, :rh2, :royalty_percentage
+  attr_accessor :rh1, :rh2, :royalty_percentage, :bi_percentage
 
-  def initialize rh1, rh2, royalty_percentage
-    @rh1, @rh2, @royalty_percentage = rh1, rh2, royalty_percentage
+  def initialize rh1, rh2, royalty_percentage, bi_percentage = nil
+    @rh1, @rh2, @royalty_percentage, @bi_percentage = rh1, rh2, royalty_percentage, bi_percentage
   end
 end
