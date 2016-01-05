61bfaa68f3bb588844d26e2a6823dbf0fff6889f
nicolasiensen <nicolas.iensen@gmail.com>
2016-01-05 14:37:36 -0200

    Initialize recording shares array in the constructor of RecordingRevenue

--- spec/aggregates/recording_revenue_spec.rb
+++ spec/aggregates/recording_revenue_spec.rb
@@ -13,4 +13,51 @@ RSpec.describe RecordingRevenue do
       )
     end.to raise_error("Recording claims can't exceed 100%")
   end
+
+  it "should initialize with no recording share" do
+    recording_revenue = RecordingRevenue.new(
+      claimed_percentage: RoyaltyPercentage.new(100.0),
+      total: 100.0,
+      recording_claims: []
+    )
+
+    expect(recording_revenue.recording_shares).to be_empty
+  end
+
+  it "should initialize with one recording share when there is one recording claim returning one recording share" do
+    recording_revenue = RecordingRevenue.new(
+      claimed_percentage: RoyaltyPercentage.new(100.0),
+      total: 100.0,
+      recording_claims: [
+        instance_double(
+          "RecordingClaim",
+          claimed_percentage: RoyaltyPercentage.new(100.0),
+          generate_recording_shares: [double("recording_share")]
+        )
+      ]
+    )
+
+    expect(recording_revenue.recording_shares.size).to be_eql(1)
+  end
+
+  it "should initialize with two recording shares when there is two recording claims returning one recording share each" do
+    recording_revenue = RecordingRevenue.new(
+      claimed_percentage: RoyaltyPercentage.new(100.0),
+      total: 100.0,
+      recording_claims: [
+        instance_double(
+          "RecordingClaim",
+          claimed_percentage: RoyaltyPercentage.new(50.0),
+          generate_recording_shares: [double("recording_share")]
+        ),
+        instance_double(
+          "RecordingClaim",
+          claimed_percentage: RoyaltyPercentage.new(50.0),
+          generate_recording_shares: [double("recording_share")]
+        )
+      ]
+    )
+
+    expect(recording_revenue.recording_shares.size).to be_eql(2)
+  end
 end
--- src/aggregates/recording_revenue.rb
+++ src/aggregates/recording_revenue.rb
@@ -2,17 +2,17 @@ class RecordingRevenue
   attr_accessor :claimed_percentage, :total, :recording_claims, :recording_shares
 
   def initialize claimed_percentage:, total:, recording_claims:
-    if recording_claims.map{|rc| rc.claimed_percentage}.inject(&:+) > 100
+    if (recording_claims.map{|rc| rc.claimed_percentage}.inject(&:+) || 0) > 100
       raise "Recording claims can't exceed 100%"
     end
 
     @claimed_percentage = claimed_percentage
     @total = total
     @recording_claims = recording_claims
+    @recording_shares = []
 
-    # @recording_shares = []
-    # recording_claims.each do |rc|
-    #   @recording_shares.concat rc.generate_recording_shares(total)
-    # end
+    recording_claims.each do |rc|
+      @recording_shares.concat rc.generate_recording_shares(total)
+    end
   end
 end
