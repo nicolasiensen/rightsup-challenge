928196222aab85f827bb02dec2571c6b8f26a56d
nicolasiensen <nicolas.iensen@gmail.com>
2016-01-05 14:40:32 -0200

    Add pseudo code for LicenseAgreement#generate_recording_shares

--- src/entities/license_agreement.rb
+++ src/entities/license_agreement.rb
@@ -5,4 +5,19 @@ class LicenseAgreement
     @rh1, @rh2, @royalty_percentage, @bi_percentage =
       rh1, rh2, royalty_percentage, bi_percentage
   end
+
+  # def generate_recording_shares total
+  #   share = total * @royalty_percentage
+  #
+  #   if bi_percentage.present?
+  #     [
+  #       RecordingShare.new(right_holder: rh1, total: share * (1 - @bi_percentage)),
+  #       RecordingShare.new(right_holder: "bi", total: share * @bi_percentage)
+  #     ]
+  #   else
+  #     [
+  #       RecordingShare.new(right_holder: rh1, total: share)
+  #     ]
+  #   end
+  # end
 end
